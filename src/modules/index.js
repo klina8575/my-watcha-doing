3d775d166f947112d06a5246bdc004025be2de0a
klina8575 <klina8575@gmail.com>
2022-05-12 18:15:04 +0900

    up

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,15 +1,17 @@
 import { combineReducers } from "redux";
 import { all } from "redux-saga/effects";
 import movie, { movieSaga } from "./movie";
+import movieDetail, { movieDetailSaga } from "./movieDetail";
 import loading from "./loading";
 
 const rootReducer = combineReducers({
   movie,
+  movieDetail,
   loading,
 });
 
 export function* rootSaga() {
-  yield all([movieSaga()]);
+  yield all([movieSaga(), movieDetailSaga()]);
 }
 
 export default rootReducer;
